7df977e2d1dd47ec8d30e6f45701e56d5c6fd487
Victoria Diana <auraiwan1756@gmail.com>
2024-01-10 21:14:49 +1100

    Add magic ball

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./About.css";
 
 function About() {
+  const [magic8Message, setMagic8Message] = useState("");
+
+  const displayMessage = () => {
+    const messages = [
+      "Nope. Not worth it.",
+      "Yes! This will happen!",
+      "Are you sure you want to do this?",
+      "I see troubles ahead. Beware!",
+      "I see a dark void inside your head. You should get that fixed.",
+      "I wouldn't bother if I were you.",
+      "So... much... haze... try again later.",
+      "You can but I wouldn't.",
+      "Let me crack open this fortune cookie first... your lucky number is 8!",
+      "Poke me like that again and your future will be very grim!",
+      "I see nothing... oh wait! Never buy a pigeon.",
+      "This will end poorly for you.",
+      "Your heart will break. Sorry.",
+      "A fool and his money will soon be parted. Leave $10 and go away.",
+      "I'd predicate something nice for you but let's be reasonable.",
+      "I can but you shouldn't.",
+      "This is your lucky day! Do it!",
+      "I feel an energy buzzing about you. This is your day!",
+    ];
+    const num = Math.floor(Math.random() * messages.length);
+    const selectedMessage = messages[num];
+    console.log(selectedMessage);
+    setMagic8Message(selectedMessage);
+  };
+
   return (
     <div>
       <h1 className="about-content">Letter to you</h1>
@@ -46,8 +75,24 @@ function About() {
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
           loading="lazy"
         ></iframe>
+        <div className="ball-content">
+        <div className="ball-heading">
+          <h1>Seeking Advice?</h1>
+          <h3>Click the Magic 8 Ball now!</h3>
+        </div>
+        <div className="magic8Ball" id="magic8Ball">
+          <img
+            src="https://dl.dropboxusercontent.com/s/3s8proa5ixmgq5k/8ball.png"
+            alt="An eight ball"
+            width="400px"
+            onClick={displayMessage}
+          />
+          <div id="magic8ballMessage" className="magic8ballMessage">
+            {magic8Message}
+          </div>
+        </div>
       </div>
-
+      </div>
     </div>
   );
 }
